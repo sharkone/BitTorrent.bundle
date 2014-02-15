9a02864780274d639eab7b159781919462c46246
Thomas Texier <sharkone@en-mousse.org>
2014-02-15 05:01:01 -0500

    Added back version selection

--- Contents/Code/yts.py
+++ Contents/Code/yts.py
@@ -1,11 +1,14 @@
 ################################################################################
 import common
+import tmdb
 
 ################################################################################
 SUBPREFIX = 'yts'
 
-YTS      = 'http://yts.re'
-YTS_LIST = YTS + '/api/list.json?limit=50&keywords={0}&genre={1}&quality={2}&set={3}'
+YTS               = 'http://yts.re'
+YTS_LIST          = YTS + '/api/list.json?limit=50&keywords={0}&genre={1}&quality={2}&set={3}'
+YTS_LIST_VERSIONS = YTS + '/api/list.json?limit=50&keywords={0}'
+YTS_MOVIE         = YTS + '/api/movie.json?id={0}'
 
 ################################################################################
 @route(common.PREFIX + '/' + SUBPREFIX + '/menu')
@@ -42,7 +45,7 @@ def search_internal(title, movie_list, query='', genre='', only_3d=False, page=1
 		if movie['Quality'] != '3D' or only_3d:
 			if movie['ImdbCode'] not in movie_list:
 				movie_list.append(movie['ImdbCode'])
-				movie_object = common.create_movie_object(movie['MovieUrl'], movie['TorrentMagnetUrl'], movie['ImdbCode'])
+				movie_object = create_movie_object(movie['MovieUrl'], movie['TorrentMagnetUrl'], movie['ImdbCode'])
 				object_container.add(movie_object)
 
 	if (page * 50) < int(json['MovieCount']):
@@ -63,3 +66,72 @@ def genres(title):
 	for genre in genres:
 		object_container.add(DirectoryObject(key=Callback(search, title=genre, genre=genre), title=genre, thumb=R('yts.png')))
 	return object_container
+
+
+################################################################################
+@route(common.PREFIX + '/' + SUBPREFIX + '/create_movie_object')
+def create_movie_object(url, magnet, imdb_id, include_container=False):
+	movie_object            = tmdb.create_movie_object(imdb_id)
+	movie_object.key        = Callback(create_movie_object, url=url, magnet=magnet, imdb_id=imdb_id, include_container=True)
+	movie_object.rating_key = url
+
+	if include_container:
+		movie_object.items = create_media_objects(url, magnet)
+
+		object_container = ObjectContainer()
+		object_container.objects.append(movie_object)
+		return object_container
+	else:
+		return movie_object
+
+
+################################################################################
+def create_media_objects(url, magnet):
+	media_objects = []
+
+	html     = HTML.ElementFromURL(url, cacheTime=CACHE_1DAY)
+	movie_id = html.xpath('//*[@class="magnet torrentDwl"]/@data-movieid')[0]
+
+	movie_data_url = YTS_MOVIE.format(movie_id)
+	movie_data     = JSON.ObjectFromURL(movie_data_url, cacheTime=CACHE_1DAY)
+
+	if movie_data['Quality'] == '3D':
+			media_object = MediaObject()
+			media_object.add(PartObject(key=Callback(common.play_torrent, url=url, magnet=magnet)))
+			media_object.video_resolution = get_closest_resolution(int(movie_data['Resolution'].partition('*')[2]))
+			media_object.video_frame_rate = movie_data['FrameRate']
+			media_objects.append(media_object)
+	else:
+		version_list_url  = YTS_LIST_VERSIONS.format(movie_data['ImdbCode'])
+		version_list_data = JSON.ObjectFromURL(version_list_url, cacheTime=CACHE_1DAY)
+
+		for version in version_list_data['MovieList']:
+			if version['Quality'] != '3D':
+				version_url  = YTS_MOVIE.format(version['MovieID'])
+				version_data = JSON.ObjectFromURL(version_url, cacheTime=CACHE_1DAY)
+
+				media_object = MediaObject()
+				media_object.add(PartObject(key=Callback(common.play_torrent, url=version_data['MovieUrl'], magnet=version_data['TorrentMagnetUrl'])))
+				media_object.video_resolution = get_closest_resolution(int(version_data['Resolution'].partition('*')[2]))
+				media_object.video_frame_rate = version_data['FrameRate']
+				media_objects.append(media_object)
+
+	media_objects.sort(cmp=media_object_compare)
+	return media_objects
+
+################################################################################
+def media_object_compare(a, b):
+	return b.video_resolution - a.video_resolution
+
+################################################################################
+def get_closest_resolution(height):
+	if height <= 240:
+		return 240
+	elif height <= 320:
+		return 320
+	elif height <= 480:
+		return 480
+	elif height <= 720:
+		return 720
+	else:
+		return 1080
