94a20741df927de25d70a515e56b7df824a61500
Thomas Texier <sharkone@en-mousse.org>
2014-02-21 00:40:23 -0500

    Can now browse KickassTorrents movies by popularity and search

--- Contents/Code/kickasstorrents_menu.py
+++ Contents/Code/kickasstorrents_menu.py
@@ -5,14 +5,21 @@ SUBPREFIX = 'kickasstorrents'
 @route(SharedCodeService.common.PREFIX + '/' + SUBPREFIX + '/menu')
 def menu():
 	object_container = ObjectContainer(title2='KickassTorrents')
-	object_container.add(InputDirectoryObject(key=Callback(search, title='Search'), title='Search', thumb=R('search.png')))
+	object_container.add(DirectoryObject(key=Callback(movies, title='Movies'), title='Movies', thumb=R('kickasstorrents.png')))
 	return object_container
 
 ################################################################################
+@route(SharedCodeService.common.PREFIX + '/' + SUBPREFIX + '/movies')
+def movies(title):
+	object_container = ObjectContainer(title2=title)
+	object_container.add(DirectoryObject(key=Callback(page, title='Popular', root='/movies', field='leechers'), title='Popular', thumb=R('kickasstorrents.png')))
+	object_container.add(InputDirectoryObject(key=Callback(search, title='Search', category='movies'), title='Search', thumb=R('search.png')))
+	return object_container
+
 ################################################################################
-@route(SharedCodeService.common.PREFIX + '/' + SUBPREFIX + '/search')
-def search(title, query=''):
-	url = SharedCodeService.kickasstorrents.KICKASSTORRENTS_SEARCH.format(String.Quote(query))
+@route(SharedCodeService.common.PREFIX + '/' + SUBPREFIX + '/page', page_index=int)
+def page(title, root, field, page_index=1):
+	url = '{0}{1}/?field={2}&sord=desc&rss=1'.format(SharedCodeService.kickasstorrents.KICKASSTORRENTS, root, field)
 	rss = RSS.FeedFromURL(url, cacheTime=0)
 
 	object_container = ObjectContainer(title2=title)
@@ -25,4 +32,12 @@ def search(title, query=''):
 		movie_object.url   = item.link
 		movie_object.title = '(S{0}|P{1}) {2}'.format(movie_seeders, movie_peers, movie_title)
 		object_container.add(movie_object)
+
+	object_container.add(NextPageObject(key=Callback(page, title=title, root=root, field=field, page_index=page_index + 1), title="More..."))
+
 	return object_container
+
+################################################################################
+@route(SharedCodeService.common.PREFIX + '/' + SUBPREFIX + '/search')
+def search(title, query, category):
+	return page(title=title, root=String.Quote('/usearch/{0} category:{1}'.format(query, category)), field='seeders')
