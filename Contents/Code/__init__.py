70ec21b1b1adb1d041bbdf5ec7bf80065868797a
Thomas Texier <sharkone@en-mousse.org>
2014-03-18 01:11:02 -0400

    First draft of new merged browsing

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -1,6 +1,5 @@
 ################################################################################
-import kickasstorrents_menu
-import thepiratebay_menu
+import movies_menu
 import yts_menu
 
 ################################################################################
@@ -10,13 +9,14 @@ ICON   = 'icon-default.png'
 
 ################################################################################
 def Start():
+	DirectoryObject.thumb  = R(ICON)
 	ObjectContainer.art    = R(ART)
 	ObjectContainer.title1 = TITLE
 	VideoClipObject.art    = R(ART)
 	VideoClipObject.thumb  = R(ICON)
 
 ################################################################################
-@handler(SharedCodeService.common.PREFIX, TITLE)
+@handler(SharedCodeService.common.PREFIX, TITLE, thumb=ICON, art=ART)
 def Main():
 	Log.Info('Server Platform: {0} ({1})'.format(Platform.OS, Platform.CPU))
 	Log.Info('Client Platform: {0}'.format(Client.Platform))
@@ -27,9 +27,7 @@ def Main():
 		object_container.header  = 'Not supported'
 		object_container.message = 'The {0} channel is not supported on {1} servers.'.format(TITLE, Platform.OS)
 	else:
-		object_container.add(DirectoryObject(key=Callback(kickasstorrents_menu.menu), title='KickassTorrents', thumb=R('kickasstorrents.png')))
-		object_container.add(DirectoryObject(key=Callback(thepiratebay_menu.menu), title='The Pirate Bay', thumb=R('thepiratebay.png')))
+		object_container.add(DirectoryObject(key=Callback(movies_menu.menu), title='Movies'))
 		object_container.add(DirectoryObject(key=Callback(yts_menu.menu), title="YTS", thumb=R('yts.png')))
-		object_container.add(PrefsObject(title='Preferences'))
 
 	return object_container
