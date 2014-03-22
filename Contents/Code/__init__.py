3502dadca80af595ded617fbe1c80b3b31460768
Thomas Texier <sharkone@en-mousse.org>
2014-03-22 12:09:25 -0400

    Added platform check when starting the plugin
    Fixes #3

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -18,9 +18,18 @@ def Start():
 ################################################################################
 @handler(SharedCodeService.common.PREFIX, TITLE)
 def Main():
+	Log.Info('Server Platform: {0} ({1})'.format(Platform.OS, Platform.CPU))
+	Log.Info('Client Platform: {0}'.format(Client.Platform))
+
 	object_container = ObjectContainer(title2=TITLE)
-	object_container.add(DirectoryObject(key=Callback(kickasstorrents_menu.menu), title='KickassTorrents', thumb=R('kickasstorrents.png')))
-	object_container.add(DirectoryObject(key=Callback(thepiratebay_menu.menu), title='The Pirate Bay', thumb=R('thepiratebay.png')))
-	object_container.add(DirectoryObject(key=Callback(yts_menu.menu), title="YTS", thumb=R('yts.png')))
-	object_container.add(PrefsObject(title='Preferences'))
+
+	if Platform.OS not in ('MacOSX', 'Windows'):
+		object_container.header  = 'Not supported'
+		object_container.message = 'The {0} channel is not supported on {1} servers.'.format(TITLE, Platform.OS)
+	else:
+		object_container.add(DirectoryObject(key=Callback(kickasstorrents_menu.menu), title='KickassTorrents', thumb=R('kickasstorrents.png')))
+		object_container.add(DirectoryObject(key=Callback(thepiratebay_menu.menu), title='The Pirate Bay', thumb=R('thepiratebay.png')))
+		object_container.add(DirectoryObject(key=Callback(yts_menu.menu), title="YTS", thumb=R('yts.png')))
+		object_container.add(PrefsObject(title='Preferences'))
+
 	return object_container
