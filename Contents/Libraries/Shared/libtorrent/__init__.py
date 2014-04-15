d51ef934373dd6d83237204ab19e1b899dfea77c
Thomas Texier <sharkone@en-mousse.org>
2014-04-15 00:56:51 -0400

    Added Linux/x86 binaries

--- Contents/Libraries/Shared/libtorrent/__init__.py
+++ Contents/Libraries/Shared/libtorrent/__init__.py
@@ -4,5 +4,7 @@ import platform
 ################################################################################
 if platform.system() == 'Darwin':
     from macosx.libtorrent import *
+elif platform.system() == 'Linux' and platform.architecture()[0] == '32bit':
+    from linux_x86.libtorrent import *
 elif platform.system() == 'Windows':
     from windows.libtorrent import *
