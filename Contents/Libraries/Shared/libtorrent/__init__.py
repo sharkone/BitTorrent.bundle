ad24a9957aff47e7d23877abf8b5b9d16d6b62a8
Stephane Jupille <tekka@TekkaServer>
2014-10-29 12:24:16 +0100

    Adding support for Linux 64 bits

--- Contents/Libraries/Shared/libtorrent/__init__.py
+++ Contents/Libraries/Shared/libtorrent/__init__.py
@@ -6,5 +6,7 @@ if platform.system() == 'Darwin':
     from macosx.libtorrent import *
 elif platform.system() == 'Linux' and platform.architecture()[0] == '32bit':
     from linux_x86.libtorrent import *
+elif platform.system() == 'Linux' and platform.architecture()[0] == '64bit':
+    from linux_x64.libtorrent import *
 elif platform.system() == 'Windows':
     from windows.libtorrent import *
