20489a69d1d9e0cf1b52d87c6b5755b035c96b3f
Thomas Texier <sharkone@en-mousse.org>
2015-03-16 23:44:47 -0400

    General: Made updater more robust

--- Contents/Code/updater.py
+++ Contents/Code/updater.py
@@ -3,46 +3,49 @@ GITHUB_REPOSITORY = 'sharkone/BitTorrent.bundle'
 
 ################################################################################
 def get_latest_version():
-    release_feed_url    = 'https://github.com/{0}/releases.atom'.format(GITHUB_REPOSITORY)
-    release_feed_data   = RSS.FeedFromURL(release_feed_url, cacheTime=0)
-    return release_feed_data.entries[0].title
+    try:
+        release_feed_url = 'https://github.com/{0}/releases.atom'.format(GITHUB_REPOSITORY)
+        release_feed_data = RSS.FeedFromURL(release_feed_url, cacheTime=0, timeout=5)
+        return release_feed_data.entries[0].title
+    except Exception as exception:
+        Log.Error('Checking for new releases failed: {0}'.format(repr(exception)))
 
 ################################################################################
 def update_available():
     latest_version = get_latest_version()
-    latest_version = latest_version[1:] if latest_version.startswith('v') else latest_version
-    return latest_version > SharedCodeService.common.VERSION
+    if latest_version:
+        return latest_version > SharedCodeService.common.VERSION
 
 ################################################################################
 def create_button():
     latest_version = get_latest_version()
-    latest_version = latest_version[1:] if latest_version.startswith('v') else latest_version
-    return DirectoryObject(key=Callback(update), title='Update available: {0}'.format(latest_version), summary='Install latest version of the BitTorrent channel.', thumb=R('update.png'))
+    if latest_version:
+        return DirectoryObject(key=Callback(update), title='Update available: {0}'.format(latest_version), summary='Install latest version of the BitTorrent channel.', thumb=R('update.png'))
 
 ################################################################################
 @route(SharedCodeService.common.PREFIX + '/update')
 def update():
     try:
         latest_version = get_latest_version()
-        zip_data       = Archive.ZipFromURL('https://github.com/{0}/archive/{1}.zip'.format(GITHUB_REPOSITORY, latest_version))
+        if latest_version:
+            zip_data = Archive.ZipFromURL('https://github.com/{0}/archive/{1}.zip'.format(GITHUB_REPOSITORY, latest_version))
 
-        SharedCodeService.scrapmagnet.stop()
-        
-        for name in zip_data.Names():
-            data    = zip_data[name]
-            parts   = name.split('/')
-            shifted = Core.storage.join_path(*parts[1:])
-            full    = Core.storage.join_path(Core.bundle_path, shifted)
+            SharedCodeService.scrapmagnet.stop()
 
-            if '/.' in name:
-                continue
+            for name in zip_data.Names():
+                data    = zip_data[name]
+                parts   = name.split('/')
+                shifted = Core.storage.join_path(*parts[1:])
+                full    = Core.storage.join_path(Core.bundle_path, shifted)
 
-            if name.endswith('/'):
-                Core.storage.ensure_dirs(full)
-            else:
-                Core.storage.save(full, data)
-        del zip_data
-        return ObjectContainer(header='Update successful', message='Channel updated to version {0}'.format(latest_version[1:] if latest_version.startswith('v') else latest_version))
+                if '/.' in name:
+                    continue
+
+                if name.endswith('/'):
+                    Core.storage.ensure_dirs(full)
+                else:
+                    Core.storage.save(full, data)
+            del zip_data
+            return ObjectContainer(header='Update successful', message='Channel updated to version {0}'.format(latest_version))
     except Exception as exception:
         return ObjectContainer(header='Update failed', message=str(exception))
-
